7d60e8b45a716f926406a824fb1d6fa3e022f5bb
Dreic <dreiceal@hotmail.com>
2022-05-25 13:30:15 -0300

    Aulas JS introdutorias

---
+++ Aula II/script.js
@@ -0,0 +1,22 @@
+/*var vOuF = false;
+var num = 1;
+var tex = "palavras";
+var arr = [0,1,2,3];
+
+
+console.log(typeof(vOuF));
+console.log(typeof(num));
+console.log(typeof(tex));
+console.log(typeof(arr));
+console.log(typeof(und));
+
+var escopoGlobal = 'Global';
+console.log(escopoGlobal);
+
+function escopoLocal(){
+
+    let escopoInterno = 'Local';
+    console.log(escopoInterno);        
+}
+
+escopoLocal();*/
